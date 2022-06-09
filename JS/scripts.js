8af6f140f7fa1365f75b0e777280fe009c70bee3
Adrincovich <adrian.drincovich@gmail.com>
2022-06-09 03:09:40 -0300

    Modal ok

--- JS/scripts.js
+++ JS/scripts.js
@@ -24,7 +24,7 @@ window.addEventListener("load", ()=> {
                 estadoValidaciones.sexo = false;
                 estadoValidaciones.interes = false;
                 estadoValidaciones.pais = false;
-                alert("Enviado exitosamente");
+                modal.style.display = "block";
                 form.reset();
             }else {
             if (estadoValidaciones.name == false && name.value == ""){
@@ -188,6 +188,27 @@ window.addEventListener("load", ()=> {
     const validaEmail = (email) => {
         return /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email);
     }
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("submit");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 })
 
 //////////////////////////////////////////////////////////////////////////////////////////
